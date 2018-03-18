d26d758c055747073856f2093a377a6f60ad671c
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-18 07:22:50 +0100

    add self.setting to AbstractBot

--- common/bot.py
+++ common/bot.py
@@ -1,9 +1,13 @@
 from common.exceptions import NotImplemented
 
 not_implemented = NotImplemented('Method has not been implemented')
+from django.conf import settings
 
 
 class AbstractObserverBot:
+    def __init__(self, exchange):
+        # exchange needs to be capitalized as it is a key in the settings.BOTS dict
+        self.setting = settings.BOTS[exchange]
 
     def create_markets(self):
         raise not_implemented
