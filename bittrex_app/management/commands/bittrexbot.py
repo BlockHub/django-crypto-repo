cef1c11330b6815d7875e98f09af58feefb22756
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-20 12:11:51 +0100

    add logging to bittrexbot

--- bittrex_app/management/commands/bittrexbot.py
+++ bittrex_app/management/commands/bittrexbot.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from bittrex_app.bot import ObserverBot
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('starting Bittrex Observerbot')
-        ObserverBot().run()
+        try:
+            self.stdout.write('starting Bittrex Observerbot')
+            ObserverBot('BITTREX').run()
+        except Exception:
+            logger.exception('Failure in bittrexbot')
