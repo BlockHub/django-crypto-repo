940580028f0a4f5847d834f598a9a5603c41c5a8
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-20 12:12:55 +0100

    add logging to gdax bot

--- gdax_app/management/commands/gdaxbot.py
+++ gdax_app/management/commands/gdaxbot.py
@@ -1,9 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from gdax_app.bot import ObserverBot
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('starting Bittrex Observerbot')
-        ObserverBot('BITTREX').run()
+        try:
+            self.stdout.write('starting Bittrex Observerbot')
+            ObserverBot('GDAX').run()
+        except Exception:
+            logger.exception('Failure in gdaxbot')
