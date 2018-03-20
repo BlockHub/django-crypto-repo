b19653fdd5eaa1417f0c3c02d28f277197bda5fc
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-20 12:10:55 +0100

    add logging to bitfinexbot

--- bitfinex_app/management/commands/bitfinexbot.py
+++ bitfinex_app/management/commands/bitfinexbot.py
@@ -1,9 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from bitfinex_app.bot import ObserverBot
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('starting Bitfinex Observerbot')
-        ObserverBot('BINANCE').run()
+        try:
+            self.stdout.write('starting Bitfinex Observerbot')
+            ObserverBot('BITFINEX').run()
+        except Exception:
+            logger.exception('Failure in bitfinexbot')
