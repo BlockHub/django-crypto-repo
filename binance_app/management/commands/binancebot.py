5d44027541ab3e0584682db717b48e36f18b23e6
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-20 12:09:57 +0100

    add logging to binancebot

--- binance_app/management/commands/binancebot.py
+++ binance_app/management/commands/binancebot.py
@@ -1,9 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
-from binance.bot import ObserverBot
+from binance_app.bot import ObserverBot
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        self.stdout.write('starting Binance Observerbot')
-        ObserverBot('BINANCE').run()
+        try:
+            self.stdout.write('starting Binance Observerbot')
+            ObserverBot('BINANCE').run()
+        except Exception:
+            logger.exception('Failure in binancebot')
+            raise
