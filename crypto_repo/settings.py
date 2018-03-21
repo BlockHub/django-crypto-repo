17534fadcb2e2d567bc25889450e75c177d1c144
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-21 17:53:01 +0100

    add locks settings and router

--- crypto_repo/settings.py
+++ crypto_repo/settings.py
@@ -88,7 +88,14 @@ WSGI_APPLICATION = 'crypto_repo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,14 +106,15 @@ if not DEBUG:
             'PORT': config('DEFAULT_DB_PORT', cast=int),
         },
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }
-    }
 
+DATABASE_ROUTERS = [
+    ...,
+    'locks.router.Router'
+]
+
+LOCKS ={
+    'db': 'default'
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
