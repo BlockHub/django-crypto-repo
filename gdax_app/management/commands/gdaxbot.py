7b88341a8c8d0d607bcbc97205ba958437bb2327
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-21 17:39:09 +0100

    add locking

--- gdax_app/management/commands/gdaxbot.py
+++ gdax_app/management/commands/gdaxbot.py
@@ -1,14 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
 from gdax_app.bot import ObserverBot
 import logging
+from locks.decorators import lock_and_log
 
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        try:
-            self.stdout.write('starting Bittrex Observerbot')
-            ObserverBot('GDAX').run()
-        except Exception:
-            logger.exception('Failure in gdaxbot')
+        @lock_and_log(logger, 'GDAX')
+        def run():
+            try:
+                self.stdout.write('starting Bittrex Observerbot')
+                ObserverBot('GDAX').run()
+            except Exception:
+                logger.exception('Failure in gdaxbot')
+        run()
