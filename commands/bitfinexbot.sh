4f6781fd84b9d8202fce7988fdcf620251d5546e
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-21 13:39:32 +0100

    add manage.py startup commands

---
+++ commands/bitfinexbot.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Need to ensure we have the correct encodings since we are initializing arky
+export LANG=en_US.UTF-8
+source /srv/CIP/venv/bin/activate
+python /srv/CIP/django-crypto-repo/manage.py bitfinexbot
